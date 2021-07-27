4c9b9368a707a79f000619620fdb2905d21b75e9
“JoselynMaM14” <2193037260@correo.ler.uam.mx>
2021-07-26 20:24:02 -0500

    Par o impar

---
+++ Par o impar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{  int n;
+   printf("Escribe el numero: ");
+   scanf("%d",&n);
+   if (n%2==0)
+   printf("El número es par");
+   else
+   printf("El número es impar");
+}
